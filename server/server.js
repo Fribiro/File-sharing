d0c8b9b4c353e64abf66c5f6a3f0b386fdcab1d6
Festus Ribiro <festusribiro@gmail.com>
2023-06-09 14:48:46 +0300

    mongo connection and file upload request

--- server/server.js
+++ server/server.js
@@ -1,10 +1,45 @@
+const multer = require("multer");
+const mongoose = require("mongoose");
 const express = require("express");
+const bcrypt = require("bcrypt");
+const File = require("./models/File");
+require('dotenv').config();
+
 const app = express();
 
-app.set("view engine", "ejs")
+const upload = multer({ dest: "uploads"})
+
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
     res.render("index")
+});
+
+app.post("/upload", upload.single("file"), async (req, res) => {
+    const fileData = {
+        path: req.file.path,
+        originalName: req.file.originalname
+    }
+    if (req.body.password !== null && req.body.password !== "") {
+        fileData.password = await bcrypt.hash(req.body.password, 10)
+    }
+
+    const file = await File.create(fileData);
+    
+    res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}`})
+});
+
+app.get("/file/:id", (req, res) => {
+
 })
 
-app.listen(3000)
+
+const PORT = process.env.PORT || 3001;
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
+})
+.catch((error) => console.log(`${error} did not connect`));
