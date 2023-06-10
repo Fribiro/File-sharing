ace395803399940642564a491832c687c784dedc
Festus Ribiro <festusribiro@gmail.com>
2023-06-10 19:22:35 +0300

    file download api

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const File = require("./models/File");
 require('dotenv').config();
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
 const upload = multer({ dest: "uploads"})
 
@@ -29,15 +30,31 @@ app.post("/upload", upload.single("file"), async (req, res) => {
     res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}`})
 });
 
-app.get("/file/:id", async (req, res) => {
+app.get("/file/:id", handleDownload);
+app.post("/file/:id", handleDownload);
+
+async function handleDownload(req, res) {
     const file = await File.findById(req.params.id);
+
+    if (file.password != null) {
+        if (req.body.password == null) {
+            res.render("password");
+            return;
+        }
+
+        if ( await bcrypt.compare(req.body.password, file.password)) {
+            res.render("password", { error: true })
+            return;
+        }
+
+    }
 
     file.downloadCount++
     await file.save();
     console.log(file.downloadCount);
 
     res.download(file.path, file.originalName);
-})
+}
 
 
 const PORT = process.env.PORT || 3001;
